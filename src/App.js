563e0399922274c3480b4a935b41adc77ad872b1
CaseySMiller <CaseySMiller@yahoo.com>
2022-08-11 03:36:24 -0600

    network test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to be as cool as me
         </a>
       </header>
     </div>
