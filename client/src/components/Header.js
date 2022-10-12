9ead327c7b2fce0a0fca9484899972d772e77c23
CaseySMiller <CaseySMiller@yahoo.com>
2022-10-11 22:05:18 -0600

    added basic navbar and mdb react

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBBtn,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBCollapse,
+} from 'mdb-react-ui-kit';
 
-const Header = () => {
+export default function App() {
+    const [showNavRight, setShowNavRight] = useState(false);
 
     return (
-        <>
-        Body
-        </>
-    );
-};
+        <MDBNavbar expand='md' light bgColor='light'>
+        <MDBContainer fluid>
+            <MDBNavbarBrand>Dice Games</MDBNavbarBrand>
 
-export default Header;
+            <MDBNavbarToggler
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavRight(!showNavRight)}
+            >
+                <MDBIcon icon='bars' fas />
+            </MDBNavbarToggler>
+
+            <MDBCollapse navbar show={showNavRight}>
+                <MDBNavbarNav right fullWidth={false} className='ms-auto mb-2 mb-lg-0'>
+                    <MDBNavbarItem>
+                    <MDBNavbarLink active aria-current='page' href='#'>
+                        Current Game
+                    </MDBNavbarLink>
+                    </MDBNavbarItem>
+
+                    <MDBNavbarItem>
+                        <MDBDropdown>
+                            <MDBDropdownToggle tag='a' className='nav-link'>
+                                Games
+                            </MDBDropdownToggle>
+                            <MDBDropdownMenu>
+                                <MDBDropdownItem link>New Farkle Game</MDBDropdownItem>
+                                <MDBDropdownItem link>New Yahtzee Game</MDBDropdownItem>
+                                <MDBDropdownItem link>Join a Game</MDBDropdownItem>
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                    </MDBNavbarItem>
+
+                    <MDBNavbarItem>
+                        <MDBDropdown>
+                            <MDBDropdownToggle tag='a' className='nav-link'>
+                                User Name
+                            </MDBDropdownToggle>
+                            <MDBDropdownMenu>
+                                <MDBDropdownItem link>View past games</MDBDropdownItem>
+                                <MDBDropdownItem link>Update User Profile</MDBDropdownItem>
+                                <MDBDropdownItem link>Log Out</MDBDropdownItem>
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                    </MDBNavbarItem>
+
+                </MDBNavbarNav>
+
+            </MDBCollapse>
+        </MDBContainer>
+        </MDBNavbar>
+    );
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MDBContainer, MDBBtn, MDBIcon, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
+const Home = () => {
+
+    return (
+        <>
+            <MDBContainer className='p-2'>
+                Body
+            </MDBContainer>
+        </>
+    );
+};
+
+export default Home;
