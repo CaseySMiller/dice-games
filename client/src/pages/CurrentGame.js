3e9239609e10f5e8e6c234620dcfeda1c4b7fc40
CaseySMiller <CaseySMiller@yahoo.com>
2023-01-12 12:09:29 -0700

    changed farkle game layout

--- client/src/pages/CurrentGame.js
+++ client/src/pages/CurrentGame.js
@@ -18,12 +18,12 @@ const CurrentGame = () => {
     return (
         <>
             <MDBContainer className='p-2'>
-                <MDBRow className='justify-content-center'>
+                <MDBRow className='d-flex justify-content-center'>
                     Current Game
                 </MDBRow>
 
-                <MDBRow className='justify-content-center'>
-                    <MDBCol md={12}>
+                <MDBRow className='d-flex justify-content-center'>
+                    <MDBCol md={9} lg={6}>
                         <div className='shadow p-0 m-0'>
                             <MDBCard className='mb-3 mx-100 hover-shadow'>
                                 <MDBCardBody>
@@ -32,110 +32,48 @@ const CurrentGame = () => {
                                         <br/>
                                         Started: 10/11/02 11:38pm
                                     </MDBCardTitle>
-                                    <MDBCardText className='text-center'>
-                                        <MDBTable small responsive align='middle'>
-                                            <MDBTableHead light>
-                                                <tr>
-                                                    <th>
-                                                        Casey M.
-                                                    </th>
-                                                    <th>
-                                                        Reno M.
-                                                    </th>
-                                                    <th>
-                                                        Cody M.
-                                                    </th>
-                                                    <th>
-                                                        April M.
-                                                    </th>
-                                                    <th>
-                                                        Dennis M.
-                                                    </th>
-                                                </tr>
-                                            </MDBTableHead>
-                                            <MDBTableBody>
-                                                <tr>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                    <td>
-                                                        1000
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        500
-                                                    </td>
-                                                    <td>
-                                                        <MDBBtn>
-                                                            Add Score
+                                    <MDBCardText >
+                                        <MDBContainer>
+                                            <MDBRow className='border-top d-flex align-items-spread'>
+                                                <MDBCol size='7' className='fs-5 d-flex align-items-center'>
+                                                    Casey Scott Miller
+                                                </MDBCol>
+                                                <MDBCol size ='3' className='fs-5 d-flex align-items-center justify-content-end'>
+                                                    1600
+                                                </MDBCol>
+                                                <MDBCol size='2' className='d-flex justify-content-end p-0'>
+                                                        <MDBBtn className='height=100% fs-3 center-all'>
+                                                            +
                                                         </MDBBtn>
-                                                    </td>
-                                                </tr>
-                                            </MDBTableBody>
-                                            <tfoot>
-                                                <tr className='table-active'>
-                                                    <td>
-                                                        <b>3500</b>
-                                                    </td>
-                                                    <td>
-                                                        <b>3000</b>
-                                                    </td>
-                                                    <td>
-                                                        <b>3000</b>
-                                                    </td>
-                                                    <td>
-                                                        <b>3000</b>
-                                                    </td>
-                                                    <td>
-                                                        <b>3000</b>
-                                                    </td>
-                                                </tr>
-                                            </tfoot>
-                                        </MDBTable>
+                                                </MDBCol>
+                                            </MDBRow>
+                                            <MDBRow className='border-top d-flex align-items-spread'>
+                                                <MDBCol size='7' className='fs-5 d-flex align-items-center'>
+                                                    Reno M.
+                                                </MDBCol>
+                                                <MDBCol size ='3' className='fs-5 d-flex align-items-center justify-content-end'>
+                                                    1600
+                                                </MDBCol>
+                                                <MDBCol size='2' className='d-flex justify-content-end p-0'>
+                                                        <MDBBtn className='height=100% fs-3 center-all'>
+                                                            +
+                                                        </MDBBtn>
+                                                </MDBCol>
+                                            </MDBRow>
+                                            <MDBRow className='border-top d-flex align-items-spread'>
+                                                <MDBCol size='7' className='fs-5 d-flex align-items-center'>
+                                                    Cody Miller
+                                                </MDBCol>
+                                                <MDBCol size ='3' className='fs-5 d-flex align-items-center justify-content-end'>
+                                                    1600
+                                                </MDBCol>
+                                                <MDBCol size='2' className='d-flex justify-content-end p-0'>
+                                                        <MDBBtn className='height=100% fs-3 center-all'>
+                                                            +
+                                                        </MDBBtn>
+                                                </MDBCol>
+                                            </MDBRow>
+                                        </MDBContainer>
                                     </MDBCardText>
                                 </MDBCardBody>
                             </MDBCard>
@@ -149,3 +87,111 @@ const CurrentGame = () => {
 };
 
 export default CurrentGame;
+
+
+
+// old code
+
+{/* <MDBTable small responsive align='middle'>
+<MDBTableHead light>
+    <tr>
+        <th>
+            Casey M.
+        </th>
+        <th>
+            Reno M.
+        </th>
+        <th>
+            Cody M.
+        </th>
+        <th>
+            April M.
+        </th>
+        <th>
+            Dennis M.
+        </th>
+    </tr>
+</MDBTableHead>
+<MDBTableBody>
+    <tr>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+    </tr>
+    <tr>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+    </tr>
+    <tr>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+        <td>
+            1000
+        </td>
+    </tr>
+    <tr>
+        <td>
+            500
+        </td>
+        <td>
+            <MDBBtn>
+                Add Score
+            </MDBBtn>
+        </td>
+    </tr>
+</MDBTableBody>
+<tfoot>
+    <tr className='table-active'>
+        <td>
+            <b>3500</b>
+        </td>
+        <td>
+            <b>3000</b>
+        </td>
+        <td>
+            <b>3000</b>
+        </td>
+        <td>
+            <b>3000</b>
+        </td>
+        <td>
+            <b>3000</b>
+        </td>
+    </tr>
+</tfoot>
+</MDBTable> */}
